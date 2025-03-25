91fa3a3a12a34fe559d7c005f35abfa5ac5ff190
gabrielvinte <gabrielphilipe212@gmail.com>
2025-03-25 09:50:09 -0300

    Update jogao.js

--- jogao.js
+++ jogao.js
@@ -2,7 +2,7 @@ const guessInput = document.getElementById('guess-input');
 const guessButton = document.getElementById('guess-button');
 const resultMessage = document.getElementById('result-message');
 
-let randomNumber = Math.floor(Math.random() * 15) + 1; // Gera o número inicialmente
+let randomNumber = Math.floor(Math.random() * 15) + 1; 
 
 guessButton.addEventListener('click', () => {
   const userGuess = parseInt(guessInput.value);
@@ -13,9 +13,7 @@ guessButton.addEventListener('click', () => {
     resultMessage.textContent = `❌ Errou! O número era ${randomNumber}. Tente novamente!`;
   }
 
-  // Reseta o número ao clicar no botão de "Enviar"
   randomNumber = Math.floor(Math.random() * 15) + 1;
 
-  // Exibe a mensagem com transição suave
   resultMessage.classList.add('show');
 });
